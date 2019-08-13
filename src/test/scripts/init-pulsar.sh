dfef3c442a100178128e2636a0e3f82eb2edeac5
Kim Christian Gaarder <kim.christian.gaarder@gmail.com>
2019-08-13 13:13:20 +0200

    More Pulsar SQL configuration updates.

--- src/test/scripts/init-pulsar.sh
+++ src/test/scripts/init-pulsar.sh
@@ -7,9 +7,9 @@ docker exec -it pulsar /pulsar/bin/pulsar-admin namespaces create test/rawdata
 docker exec -it pulsar /pulsar/bin/pulsar-admin namespaces set-retention --size -1 --time -1 test/rawdata
 
 # Pulsar SQL commands
-docker exec -it pulsar apt-get update && apt-get install less
+docker exec -it pulsar apt-get update && docker exec -it pulsar apt-get install less
 docker exec -it pulsar touch /root/.presto_history
 docker exec -it pulsar /pulsar/bin/pulsar sql-worker start
 
 # Test Pulsar SQL (Presto) using interactive shell
-#/pulsar/bin/pulsar sql
+#docker exec -it pulsar /pulsar/bin/pulsar sql
